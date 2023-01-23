cc29aa67c13e99d5a58f4a32b99d0e3a3c9890a8
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2023-01-23 09:24:50 +0100

    Update urls.py

--- Projekt_PSI/lekcje/urls.py
+++ Projekt_PSI/lekcje/urls.py
@@ -1,31 +1,9 @@
 from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path, include
-from . import views
-
-
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('onas/', views.onas),
-    path('', views.strona),
-    url(r'^api-auth/', include('rest_framework.urls')),
-
-    path('Nauczyciele',views.NauczycieleList.as_view(), name=views.NauczycieleList.name),
-    path('Nauczyciele/<int:pk>',views.NauczycieleDetail.as_view(), name=views.NauczycieleDetail.name),
-    path('LekcjeNauczycieli', views.LekcjeNauczycieliList.as_view(), name=views.LekcjeNauczycieli.name),
-    path('LekcjeNauczycieli/<int:pk>', views.LekcjeNauczycieliDetail.as_view(), name=views.LekcjeNauczycieli.name),
-    path('Lekcje', views.Lekcje.as_view(), name=views.LekcjeList.name),
-    path('Lekcje/<int:pk>', views.LekcjeDetail.as_view(), name=views.LekcjeDetail.name),
-    path('LekcjeUczniow', views.LekcjeUczniowList.as_view(), name=views.LekcjeUczniow.name),
-    path('LekcjeUczniow/<int:pk>', views.LekcjeUczniowDetail.as_view(), name=views.LekcjeUczniow.name),
-    path('Platnosci', views.PlatnosciList.as_view(), name=views.PlatnosciList.name),
-    path('Platnosci/<int:pk>', views.PlatnosciDetail.as_view(), name=views.PlatnosciDetail.name),
-    path('EgzaminyUczniow', views.EgzaminyUczniowList.as_view(), name=views.EgzaminyUczniowList.name),
-    path('EgzaminyUczniow/<int:pk>', views.EgzaminyUczniowDetail.as_view(), name=views.EgzaminyUczniowDetail.name),
-    path('Egzaminy', views.EgzaminyList.as_view(), name=views.EgzaminyList.name),
-    path('Egzaminy/<int:pk>', views.EgzaminyDetail.as_view(), name=views.EgzaminyDetail.name),
-    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
-
+    path('api-auth/', include('rest_framework.urls')),
+    path('',include('projekt.urls')),
 ]
+
